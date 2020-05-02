988f6fba847a13a99dc0d61517e82d3f14711520
Michael Fink <michael.fink@asamnet.de>
2020-05-02 12:42:06 +0200

    added module and node icons

--- src/ProgrammersGlasses/userinterface/resource.h
+++ src/ProgrammersGlasses/userinterface/resource.h
@@ -4,12 +4,15 @@
 //
 #define IDR_MAINFRAME                   128
 #define IDD_ABOUTBOX                    129
+#define IDB_MODULE_COFF                 512
+#define IDB_NODE_DOCUMENT               1024
+#define IDB_NODE_LIBRARY                1025
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_RESOURCE_VALUE        1026
 #define _APS_NEXT_COMMAND_VALUE         32775
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           102
