38f70ba6c0c21f773859d7ec9bd59d59aeb30284
Michael Fink <michael.fink@asamnet.de>
2020-05-09 19:38:21 +0200

    added helper method to get a value from an std::map

--- src/ProgrammersGlasses/Helper.hpp
+++ src/ProgrammersGlasses/Helper.hpp
@@ -8,6 +8,15 @@
 #pragma once
 
 #include <vector>
+#include <map>
 
 /// Splits a string by one or more split characters
 std::vector<CString> StringSplit(const CString& text, LPCTSTR splitChars, bool addEmptyEntries);
+
+/// Returns value from a std::map with given key, or a default value when the key is not found in the map
+template <typename T>
+inline LPCTSTR GetValueFromMapOrDefault(const std::map<T, LPCTSTR>& map, const T key, LPCTSTR defaultValue)
+{
+   auto iter = map.find(key);
+   return iter != map.end() ? iter->second : defaultValue;
+}
