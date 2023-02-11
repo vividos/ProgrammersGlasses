4dc455fe3b2ca9bd34da74b399b6f880e165962b
Michael Fink <michael.fink@asamnet.de>
2023-02-11 10:01:17 +0100

    allow FilterSortListViewNode to have sub nodes

--- src/ProgrammersGlasses/modules/FilterSortListViewNode.hpp
+++ src/ProgrammersGlasses/modules/FilterSortListViewNode.hpp
@@ -24,12 +24,6 @@ public:
       bool allowFiltering);
 
    // Inherited via INode
-   virtual const std::vector<std::shared_ptr<INode>>& ChildNodes() const override
-   {
-      static std::vector<std::shared_ptr<INode>> emptyNodeList;
-      return emptyNodeList;
-   }
-
    virtual std::shared_ptr<IContentView> GetContentView() override;
 
 private:
